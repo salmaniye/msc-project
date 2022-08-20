342387f7b6af8a8f6bbbdd4773640d7b4cae03a6
Salman Fatahillah <fatahillah.salman@gmail.com>
2022-08-20 17:53:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ plots = st.container()
 kw_search = st.container()
 common = st.container()
 
-#@st.cache
+@st.experimental_memo
 def call_dataset(game_name):
 	# load data
 	game_csv = game_name
@@ -61,13 +61,18 @@ def call_dataset(game_name):
 	data.reset_index(drop=True, inplace=True)
 	return data
 
+# def create_timeseries_plot:
+# 	return
+
+# def create_area_plot:
+
+
 with header:
 	st.title("Sentiment of gamers' pre-release tweets on main series Pokémon games")
-	st.markdown('The main series of Pokémon games are one of the highest selling video games of all time')
+	st.caption('**Please note the controls are on the sidebar**')
+	st.markdown('This is a web app that displays tweets and their sentiment on the selected Pokémon game')
 
 with dataset:
-	st.header('Tweets on Pokémon games')
-
 	games_list = ['Pokémon X&Y', 'Pokémon Omega Ruby & Alpha Sapphire',
 				  'Pokémon Sun & Moon', 'Pokémon Ultra Sun & Ultra Moon',
 				  "Pokémon Let's Go, Pikachu! and Let's Go, Eevee!",
@@ -80,9 +85,12 @@ with dataset:
 	games_dict = dict(games_zip)
 
 	# a dropdown for the user to choose the game to be displayed
-	game_name = st.selectbox('Select a game to display:', games_list)
-
-	st.write('You have selected:', game_name)
+	### SIDEBAR
+	st.sidebar.markdown("# Controls")
+	game_name = st.sidebar.selectbox('Select a game to display:', games_list)
+	
+	st.header(f'Tweets on {game_name}')
+	st.sidebar.caption(f'You have selected: {game_name}')
 
 	game_dataset = call_dataset(games_dict[game_name])
 
@@ -90,7 +98,9 @@ with dataset:
 	sentiment_per_day = game_dataset.groupby(['sentiment','date'], as_index=False).size()
 	min_date = sentiment_per_day['date'].min()
 	max_date = sentiment_per_day['date'].max()
-	date_range = st.slider('date range', min_date, max_date, (min_date, max_date))
+
+	### SIDEBAR
+	date_range = st.sidebar.slider('Please select the range of dates:', min_date, max_date, (min_date, max_date))
 
 	# add a slider for user to input
 	slider_df = sentiment_per_day[sentiment_per_day['date'].between(date_range[0],date_range[1],inclusive=True)]
@@ -100,20 +110,37 @@ with plots:
 	date_range_df = game_dataset[game_dataset['date'].between(date_range[0],date_range[1],inclusive=True)]
 	st.text(f'Tweets from {date_range[0]} to {date_range[1]}')
 	game_dataset_clean = date_range_df[['text','date','sentiment scores','sentiment']]
-	st.dataframe(game_dataset_clean)
+
+	# option to filter by sentiment
+	### SIDEBAR
+	options_sentiment = st.sidebar.multiselect(label='Filter by sentiment (dropdown):',
+		options=['Positive', 'Neutral', 'Negative'],
+		default=['Positive', 'Neutral', 'Negative'])
+	filtered_df = game_dataset_clean[game_dataset_clean["sentiment"].isin(options_sentiment)]
+	st.dataframe(filtered_df)
 
 	# fig1. sentiment per day
 	st.header(f"Sentiment on {game_name} per day")
-	fig = px.line(slider_df, x='date', y='size', title='Number of tweets their sentiment over time', 
-		color='sentiment', color_discrete_sequence=['#DC3912', '#3366CC', '#109618']) #['red', 'blue', 'green']
+	slider_df = slider_df[slider_df["sentiment"].isin(options_sentiment)]
+	fig = px.line(slider_df, x='date', y='size', labels={
+		'date':'Date',
+		'size':'Number of tweets',
+		'sentiment':'Sentiment'},
+		title='Number of tweets and their sentiment over time', color='sentiment',
+		color_discrete_map={'Negative':'#DC3912','Neutral':'#3366CC','Positive':'#109618'}) 
+		#['red', 'blue', 'green']
 	st.write(fig)
 
 	# fig2. normalized sentiment area per day
 	sentiment_total_pd = sentiment_per_day.groupby(['date'], as_index=False).sum()
 	spd = sentiment_per_day.merge(sentiment_total_pd, left_on = 'date', right_on='date')
 	spd['sentiment percentage'] = spd['size_x']/spd['size_y']
-	fig2 = px.area(spd, x='date', y='sentiment percentage', title='Normalized sentiment per day',
-				 color='sentiment', color_discrete_sequence=['#DC3912', '#3366CC', '#109618'])
+	fig2 = px.area(spd, x='date', y='sentiment percentage',labels={
+		'date':'Date',
+		'sentiment percentage':'Sentiment (%)',
+		'sentiment':'Sentiment'},
+		title='Normalized sentiment per day', color='sentiment',
+		color_discrete_map={'Negative':'#DC3912','Neutral':'#3366CC','Positive':'#109618'})
 	st.write(fig2)
 
 	# area plot
@@ -123,19 +150,23 @@ with plots:
 	
 	# st.write(fig_area)
 
-with kw_search:
+# with kw_search:
 
-	# search keyword in dataframe
-	keyword = st.text_input('Search keyword within the date range (case insensitive):')
-	st.write('The current keyword is:', keyword)
+# 	# search keyword in dataframe
+# 	### SIDEBAR
+# 	keyword = st.sidebar.text_input('Search keyword within the date range (case insensitive):')
+# 	st.sidebar.markdown(f'The current keyword is: {keyword}')
 
-	keyword_df = date_range_df[date_range_df['text'].str.contains(pat=keyword, case=False)==True]
-	st.dataframe(keyword_df)
 
-	keyword_per_day = keyword_df.groupby(['sentiment','date'], as_index=False).size()
-	fig_kw = px.line(slider_df, x='date', y='size', title=f'Number of tweets with "{keyword}" and their sentiment over time', 
-		color='sentiment', color_discrete_sequence=['#DC3912', '#3366CC', '#109618']) #['red', 'blue', 'green']
-	st.write(fig_kw)
+# 	keyword_df = date_range_df[date_range_df['text'].str.contains(pat=keyword, case=False)==True]
+# 	st.dataframe(keyword_df)
+
+# 	keyword_per_day = keyword_df.groupby(['sentiment','date'], as_index=False).size()
+# 	fig_kw = px.line(slider_df, x='date', y='size',
+# 		title=f'Number of tweets with "{keyword}" and their sentiment over time', color='sentiment',
+# 		color_discrete_map={'Negative':'#DC3912','Neutral':'#3366CC','Positive':'#109618'})
+# 		#['red', 'blue', 'green']
+# 	st.write(fig_kw)
 
 with common:
 	# most common words
@@ -152,7 +183,8 @@ with common:
 	# 	dataset_text = dataset_text.replace(word,'')
 
 	fig_word, ax = plt.subplots()
-	wordcloud = WordCloud(background_color='white', colormap='Set2', collocations=False, stopwords = STOPWORDS).generate(dataset_text)
+	wordcloud = WordCloud(background_color='white', colormap='Set2',
+				collocations=False, stopwords = STOPWORDS).generate(dataset_text)
 
 	st.header('Word cloud of most common words')
 	plt.imshow(wordcloud, interpolation='bilinear')
